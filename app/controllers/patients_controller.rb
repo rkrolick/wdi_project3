a78e036c1051bcb90845d42e363099ddd5a62ad9
Ryan Krolick <rkrolick@gmail.com>
2016-04-04 11:16:12 -0400

    cleaned patient controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,10 +1,9 @@
 class PatientsController < ApplicationController
   def index
-    respond_to do |format|
-      format.html
-      format.json{ render json: Patient.all }
-    end
+      @patients = Patient.all
+      render json: @patients.to_json, status: :ok
   end
+
   def show
     @patient = Patient.find(params[:id])
     render json: @patient.to_json, status: :ok
@@ -14,10 +13,12 @@ class PatientsController < ApplicationController
     Patient.destroy(params[:id])
     render json: {success: true}, status: :ok
   end
+  
   def create
     @patient = Patient.create(patient_params)
-    render json: @patient, status: :ok
+    render json: @patient.to_json, status: :ok
   end
+
   private
   def patient_params
     params.require(:patient).permit(:name, :image_url, :cost, :quantity, :country)
