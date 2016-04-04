bddaaa6a7ea3524154081f64d1aa1cd4521a741c
Ryan Krolick <rkrolick@gmail.com>
2016-04-04 11:45:16 -0400

    cleanup patients controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -9,18 +9,8 @@ class PatientsController < ApplicationController
     render json: @patient.to_json, status: :ok
   end
 
-  def destroy
-    Patient.destroy(params[:id])
-    render json: {success: true}, status: :ok
-  end
-  
-  def create
-    @patient = Patient.create(patient_params)
-    render json: @patient.to_json, status: :ok
-  end
-
   private
   def patient_params
-    params.require(:patient).permit(:name, :image_url, :cost, :quantity, :country)
+    params.require(:patient).permit()
   end
 end
