f882983a3e94f020345dc0598b693670449dfdfa
Mohamed Chalal <Mohamedrchalal@gmail.com>
2016-04-04 18:48:20 -0400

    added 'at a glance' version 1'

--- app/assets/javascripts/patient.index.controller.js
+++ app/assets/javascripts/patient.index.controller.js
@@ -12,6 +12,7 @@
   function PatientControllerFunction(PatientFactory){
     var vm = this;
     vm.patients = PatientFactory.query();
+    vm.active = 0;
   };
     $('#activeButton').on('click', function(){
       console.log(vm.active);
