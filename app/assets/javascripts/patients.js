d126b87efa3608de535922697af9c2e88213ddd3
Mohamed Chalal <Mohamedrchalal@gmail.com>
2016-04-04 12:20:13 -0400

    fixed view kind of

--- app/assets/javascripts/patients.js
+++ app/assets/javascripts/patients.js
@@ -4,6 +4,26 @@
 
 (function(){
   angular
-  .module("handOff",["patientController","patientFactory"])
-
+  .module("patients",["patientFactory"])
+  .controller("patientController", PatientControllerFunction);
+  PatientController.$inject = ["$resource"]
+  function PatientControllerFunction($resource){
+    var vm = this;
+    vm.patients = $resource("/patients.json", {}, {
+      update: {method: 'PUT'}
+    });
+    // vm.destroy = function(patient_index){
+    //   var patient = vm.data[patient_index];
+    //   Patient.remove({id: patient.id}, function(response){
+    //     if(response.success) vm.data.splice(patient_index, 1);
+    //   });
+    // }
+    vm.new_patient = {};
+    vm.create = function(){
+      Patient.save(vm.new_patient, function(response){
+        vm.data.push(response);
+        vm.new_patient = {};
+      })
+    }
+  }
 }());
