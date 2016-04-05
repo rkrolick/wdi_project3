039bf11f479c35e2be1b9633f56f10603dc98c11
Vijender Singh Bajwa <vbajwamd@gmail.com>
2016-04-05 14:01:58 -0400

    updated patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,6 +1,7 @@
 class Patient < ActiveRecord::Base
   has_many :visits, dependent: :destroy
   has_many :vitals, through: :visits
+  has_many :cbc,through: :visits
   serialize :pmh
   serialize :homeMeds
   serialize :surgicalHx
