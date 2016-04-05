6d489f791f69636787c249a9d2d1d6901d0938c9
Ryan Krolick <rkrolick@gmail.com>
2016-04-05 14:54:23 -0400

    vitals now have full CRUD functionality

--- app/controllers/vitals_controller.rb
+++ app/controllers/vitals_controller.rb
@@ -1,8 +1,58 @@
 class VitalsController < ApplicationController
   def index
-    @patient = Patient.find(params[:patient_id])
-    @visit = @patient.visits.find(params[:visit_id])
+    get_visit
     @vitals = @visit.vitals.all
     render json: @vitals.to_json, status: :ok
   end
+
+  def show
+    get_visit
+    @vital = @visit.vitals.find(params[:id])
+    render json: @vital.to_json, status: :ok
+  end
+
+  def create
+    get_visit
+    @vital = @visit.vitals.new(vital_params)
+    if @vital.save
+      render json: @vital.to_json, status: :created
+    else
+      render json: @vital.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    get_visit
+    @vital = @visit.vitals.find(params[:id])
+    if @vital.update(vital_params)
+      render json: @vital.to_json, status: :ok
+    else
+      render json: @vital.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    get_visit
+    @vital = @visit.vitals.find(params[:id])
+    @vital.destroy
+    render json: {status: "success"}, status: :ok
+  end
+
+  private
+  def vital_params
+    params.require(:visit).permit(
+    :o2Sat,
+    :temp,
+    :heartRate,
+    :systolic,
+    :diastolic,
+    :respiratoryRate
+    )
+  end
+
+  def get_visit
+    @patient = Patient.find(params[:patient_id])
+    @visit = @patient.visits.find(params[:visit_id])
+  end
+
 end
