12f82ef0e1376acc34d3e26cbbc14cabcf765995
Vijender Singh Bajwa <vbajwamd@gmail.com>
2016-04-06 12:33:11 -0400

    added seeds for dx

--- db/migrate/20160406143142_create_dxs.rb
+++ db/migrate/20160406143142_create_dxs.rb
@@ -1,7 +1,6 @@
 class CreateDxs < ActiveRecord::Migration
   def change
     create_table :dxs do |t|
-      t.string :api_name
       t.string :code
       t.string :description
       t.timestamps null: false
