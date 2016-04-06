289c7e3ad7eb7dbf10b3d917d9f7760cbde37f9b
Ryan Krolick <rkrolick@gmail.com>
2016-04-06 11:10:46 -0400

    patient index controller updated to provide visits

--- app/assets/javascripts/patient.index.controller.js
+++ app/assets/javascripts/patient.index.controller.js
@@ -5,16 +5,19 @@
   .module("handOff")
   .controller("PatientIndexController", [
     "PatientFactory",
+    "VisitFactory",
     "$resource",
     PatientControllerFunction
   ]);
 
-  function PatientControllerFunction(PatientFactory){
+  function PatientControllerFunction(PatientFactory, VisitFactory){
     var vm = this;
     vm.patients = PatientFactory.query();
     vm.active = 0;
     vm.setActivePatient = function(pat) {
       vm.activePatient = pat
+      vm.visits = VisitFactory.query({patient_id: vm.activePatient.id});
+      console.log(vm.visits);
     };
     vm.test = function(){
       console.log(vm.hide)
